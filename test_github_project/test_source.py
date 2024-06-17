fdc6d117a18a6e9a9f55cc195c2bafe6a177487e
1v_All <developer_1v@proton.me>
2024-06-17 15:09:47 -0700

    initial

---
+++ test_github_project/test_source.py
@@ -0,0 +1,13 @@
+'''
+    This file is kept simple so that requirements generation and 
+    freezing/downloading/installing files is fast
+    
+    '''
+    
+from print_tricks import pt
+import math, sys, os
+
+
+pt('From inside of test_source_simple!')
+
+
